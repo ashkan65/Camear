74cf47737fb9b8b330ecf24334bbe9397feb7ead
Ashkan Pourkand <ashkan.pourkand@gmail.com>
2020-06-22 16:03:22 -0700

    Add files via upload

--- CameraCalibration/ObjectTracking/3DTracking/main.cpp
+++ CameraCalibration/ObjectTracking/3DTracking/main.cpp
@@ -70,10 +70,12 @@ int main () {
     cal_file["R1"] >> Rect1;
     cal_file["R2"] >> Rect2;
 // std::cout<< Rect2;
-    cv::Mat pnts3D(4,1,CV_64F);
+    cv::Mat pnts3D(1,1,CV_64F);
 	//////////////////////////////////////////////
 	TelerobCamera cam1(pCameras[0]);
 	TelerobCamera cam2(pCameras[1]);
+
+    cv::Point Pt1, Pt2;
 	char key = 0;
     while(key != 'q')
     {
@@ -82,14 +84,15 @@ int main () {
         cam1.FilterCurrentFrame();
         cam2.FilterCurrentFrame();
         std::cout <<"This is one frame : ";
-        std::vector<cv::Point2d> cam0pnts;
-        std::vector<cv::Point2d> cam1pnts;
-        // cam0pnts[0] =  cam1.GetTargetPose();
-        // cam1pnts[0] =  cam2.GetTargetPose();
-        // cv::triangulatePoints(Projection1,Projection2,cam0pnts,cam1pnts,pnts3D);
+        Pt1 = cam1.GetTargetPose();
+        Pt2 = cam2.GetTargetPose();
+        cv::Mat Taret1 = (cv::Mat_<double>(2,1) << Pt1.x, Pt1.y);
+        cv::Mat Taret2 = (cv::Mat_<double>(2,1) << Pt2.x, Pt2.y);
+
+        cv::triangulatePoints(Projection1,Projection2,Taret1,Taret2,pnts3D);
 
         // std::cout << cam1.GetTargetPose();
-        // std::cout << pnts3D<<std::endl;
+        std::cout << pnts3D<<std::endl;
         cv::imshow("image1", cam1.GetCurrentFrame()); 
         // cv::imshow("Object Detection1", cam1.GetCurrentFilteredFrame());
         // cv::imshow("Object Detection2", cam2.GetCurrentFilteredFrame());
